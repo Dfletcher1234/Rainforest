b4f5cac6c9acc8a6b3b28c6972cd55ae9e3d66bf
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-07 11:20:14 -0400

    created CRUD methods in products_controller

---
+++ rainforest/app/controllers/products_controller.rb
@@ -0,0 +1,36 @@
+class ProductsController < ApplicationController
+
+  def create
+    @
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
