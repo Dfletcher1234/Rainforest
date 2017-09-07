a15d7974b87595114d7d49d2c0aab6126e72857a
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-07 11:44:26 -0400

    added seed data for product

--- rainforest/db/seeds.rb
+++ rainforest/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(name: "lawnmower", description: "Fantastic green grass-cutting machine.", price_in_cents: 100000 )
+Product.create!(name: "Leaf Blower", description: "Fantastic lean-blowing machine.", price_in_cents: 200000)
+Product.create!(name: "Ipod", description: "Listen to your tunes", price_in_cents: 400000)
+Product.create!(name: "laptop", description: "slow, old, dated, and dusty.", price_in_cents: 10000)
+Product.create!(name: "Fan", description: "Hot to trot, cool yourself down with this fan", price_in_cents: 500000 )
