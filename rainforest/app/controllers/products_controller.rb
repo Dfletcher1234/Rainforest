4e53ec1819d75d81a4ed95ae9561c65b03a17560
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-07 16:22:50 -0400

    edit form works and redirects to products page with edits

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -30,9 +30,22 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+
+
+
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+    if @product.save
+      redirect_to products_path
+    else
+      render :edit
+    end
+
   end
 
   def destroy
